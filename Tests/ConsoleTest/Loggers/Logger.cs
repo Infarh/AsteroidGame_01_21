a13baf2e71877c69b40a43060dd92c18adf4bdd2
Infarh <shmachilin@yandex.ru>
2021-01-14 19:53:25 +0300

    Текстовый логгер

---
+++ Tests/ConsoleTest/Loggers/Logger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleTest.Loggers
+{
+    internal abstract class Logger
+    {
+        public abstract void Log(string Message);
+
+        public void LogInformation(string Message)
+        {
+            Log($"{DateTime.Now:s}[info]:{Message}");
+        }
+
+        public void LogWarning(string Message)
+        {
+            Log($"{DateTime.Now:s}[warning]:{Message}");
+        }
+
+        public void LogError(string Message)
+        {
+            Log($"{DateTime.Now:s}[error]:{Message}");
+        }
+
+        public void LogCritical(string Message)
+        {
+            Log($"{DateTime.Now:s}[critical]:{Message}");
+        }
+
+        public abstract void Flush();
+    }
+}
--- Tests/ConsoleTest/Program.cs
+++ Tests/ConsoleTest/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace ConsoleTest
+﻿using System;
+using ConsoleTest.Loggers;
+
+namespace ConsoleTest
 {
     class Program
     {
+        private static int _X = 5;
+        private static int _Y = 0;
+        private static Logger __Logger;
+
         static void Main(string[] args)
         {
-            
+            __Logger = new TextFileLogger("test.log");
+
+            __Logger.LogInformation("Приложение запущено");
+
+            try
+            {
+                var z = _X / _Y;
+            }
+            catch (DivideByZeroException)
+            {
+                __Logger.LogError($"Ошибка при делении {_X} на 0");
+            }
+
+            Console.WriteLine("Нажмите Enter для выхода.");
+            Console.ReadLine();
+
+            __Logger.LogInformation("Работа приложения завершена");
+
+            __Logger.Flush();
         }
     }
 }

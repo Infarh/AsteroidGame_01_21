1cdbb5db5202e07f77db48cb8c4009263405815f
Infarh <shmachilin@yandex.ru>
2021-01-14 19:31:51 +0300

    Чистка хлама

---
+++ Tests/ConsoleTest/Vector2D.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleTest
+{
+    struct Vector2D
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public double Length => Math.Sqrt(X * X + Y * Y);
+
+        //public Vector2D()
+        //{
+
+        //}
+
+        public Vector2D(double X, double Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+    }
+}

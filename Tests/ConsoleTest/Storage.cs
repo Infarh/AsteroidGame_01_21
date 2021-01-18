a8cefb29427a7ca4740cd49672117433ccb09138
Infarh <shmachilin@yandex.ru>
2021-01-18 19:42:07 +0300

    Обобщённое хранилище объектов

---
+++ Tests/ConsoleTest/Storage.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleTest
+{
+    abstract class Storage<TItem>
+    {
+        private readonly List<TItem> _Items = new List<TItem>();
+
+        public virtual int Count => _Items.Count;
+
+        public virtual TItem this[int Index]
+        {
+            get
+            {
+                return _Items[Index];
+            }
+            set
+            {
+                if(_Items.Contains(value)) return;
+                _Items[Index] = value;
+            }
+        }
+
+        public virtual void Add(TItem item)
+        {
+            if (_Items.Contains(item)) return;
+            _Items.Add(item);
+        }
+
+        public virtual bool Remove(TItem item)
+        {
+            return _Items.Remove(item);
+        }
+
+        public virtual bool IsContains(TItem item) => _Items.Contains(item);
+
+        public virtual void Clear() => _Items.Clear();
+
+        public abstract void SaveToFile(string FileName);
+
+        public virtual void LoadFromFile(string FileName)
+        {
+            Clear();
+        }
+    }
+}
--- Tests/ConsoleTest/Student.cs
+++ Tests/ConsoleTest/Student.cs
@@ -4,6 +4,8 @@ namespace ConsoleTest
 {
     class Student
     {
+        public int Id { get; init; }
+
         public string Name { get; init; }
 
         public string Surname { get; init; }
